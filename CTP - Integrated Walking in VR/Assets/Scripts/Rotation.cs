7db4b886114c1ca361c882895b991262f9fac789
Aaron Drinkwater <Aaron3.Drinkwater@live.uwe.ac.uk>
2020-01-30 02:33:02 +0000

    Started Making my own very basic redirected walking in a new scene.
    Needs testing in VR, All of it.

---
+++ CTP - Integrated Walking in VR/Assets/Scripts/Rotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public Transform player;
+    public Transform target;
+
+    public GameObject tmp_target;
+
+    public bool no_tmp_target;
+
+    private float rotationFromCurvatureGain; //Proposed curvature gain based on user speed
+    private float rotationFromRotationGain; //Proposed rotation gain based on head's yaw
+    private float lastRotationApplied = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CTP - Integrated Walking in VR/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    private float force = 10.0f;
+    private float speed = 4.0f;
+
+    public float acceler;
+
+    bool turn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        acceler = GetComponent<OVRPlayerController>().Acceleration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //private void OnCollisionEnter(Collision c)
+    //{
+    //    if(c.gameObject.tag == "Waypoint")
+    //    {
+    //        Vector3 dir = c.contacts[0].point - transform.position;
+    //        dir = -dir.normalized;
+
+    //        GetComponent<Rigidbody>().AddForce(dir*force);
+
+    //        Debug.Log("I have entered the collider");
+    //    }
+    //}
+
+    void OnTriggerEnter(Collider other)
+    {
+        turn = true;
+        if (turn = true && other.gameObject.tag == "Waypoint")
+        {
+            //gameObject.transform.Rotate(0, 90, 0);
+            //gameObject.transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 90, 0), speed * Time.deltaTime);
+            gameObject.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 90, 0), Time.deltaTime * speed);
+            //transform.rotation = Quaternion.LookRotation();
+
+            Debug.Log("I have entered the collider");
+        }
+
+        turn = false;
+    }
+
+}
